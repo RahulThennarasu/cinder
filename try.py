ac5db1a38be8b1375e2f50a4007817640bd58fc5
Rahul Thennarasu <rahulthennarasu07@gmail.com>
2025-05-28 22:11:19 -0700

    ignore firebase creds

--- try.py
+++ try.py
@@ -11,7 +11,7 @@ import numpy as np
 
 from backend.model_interface.connector import ModelDebugger
 
-CINDER_API_KEY = "cinder_1748479894241_i9fsbitv79"
+CINDER_API_KEY = "cinder_1748495376549_w1i5sdgp34d"
 
 # Define a more complex model than the original simple example
 class NeuralNetwork(nn.Module):
